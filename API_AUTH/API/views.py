f6c7ab8177c4151e26b13275f5181b46a9bf7a84
Alan Espinoza <alan.espinoza@gmail.com>
2022-12-05 17:27:41 -0300

    Change in Folders Order

---
+++ API_AUTH/API/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.http.response import JsonResponse
+from .basic_auth import request_auth
+# Create your views here.
+
+class Test(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self,request):
+        auth_response = request_auth(request)
+        try:
+            auth_response[0]
+            return JsonResponse(auth_response,status=200,safe=False)
+        except:
+            if auth_response['message-error']=='You have to authenticate':
+                return JsonResponse(auth_response,status=500,safe=False)
+            else:
+                return JsonResponse(auth_response,status=404,safe=False)
--- Django-BasicAuth/API_AUTH/API/urls.py
+++
@@ -1,8 +0,0 @@
-from .views import Test
-from django.urls import path
-
-
-urlpatterns = [
-    path('test/',Test.as_view(),name='test'),
-    
-]
